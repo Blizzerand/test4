45668832ab515e11c2cc28afd19dd37882149ea2
SanTrans <anon.wiki04@gmail.com>
2013-04-27 18:43:45 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test4::Application.config.secret_token = 'de0e48cab86df27312e578de7fdf6f5640be512ad4e22e5dc9b2c7fb402955ef5ce0142d05fdda31698cd651e00cc4d16e7b07196b20c90b6d748f150b05b108'
